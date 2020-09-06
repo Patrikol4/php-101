44c9e6f526cd2af248f14cc46ff6db56d670126c
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-09-06 19:21:14 -0300

    Classes e seus atributos
    Neste arquivo você poderá ter uma noção melhor do que uma classe pode oferecer

---
+++ classes-com-atributos.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Pessoa 
+{
+  public $nome;
+  public $idade;
+  public $profissao;
+  public $escolaridade;
+  
+  public function mostrarNome()
+  {
+    return $this->nome;
+  }
+}
+
+?>
