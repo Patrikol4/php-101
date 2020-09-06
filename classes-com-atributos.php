dfa87625b26df5f84c8084a27b8b1b78f2e0eec0
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-09-06 19:27:28 -0300

    Update classes-com-atributos.php

--- classes-com-atributos.php
+++ classes-com-atributos.php
@@ -6,11 +6,43 @@ class Pessoa
   public $idade;
   public $profissao;
   public $escolaridade;
+  public $rendaMensal;
   
   public function mostrarNome()
   {
     return $this->nome;
   }
+  
+  public function mostrarIdade()
+  {
+   return $this->idade;
+  }
+  public function mostrarProfissao()
+  {
+    return $this->profissao;
+  }
+  public function mostrarEscolaridade()
+  {
+    return $this->escolaridade;
+  }
+  public function mostrarRenda()
+  {
+    return $this->rendaMensal;
+  }
 }
+
+$pessoa = new Pessoa();
+
+$pessoa->nome = "Joao da Silva Cavalcante";
+$pessoa->idade = 24;
+$pessoa->profissao = "Autonomo";
+$pessoa->escolaridade = "Ensino medio completo";
+$pessoa->rendaMensal = "2400 R$";
+print $pessoa->nome;
+print $pessoa->idade;
+print $pessoa->profissao;
+print $pessoa->escolaridade;
+print $pessoa->rendaMensal;
+
 
 ?>
